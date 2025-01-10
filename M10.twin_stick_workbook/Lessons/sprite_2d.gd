d0ede9c408de4900c9645bb32672b833fec91e73
yaliu10 <yaliu10@s.sfusd.edu>
2025-01-10 15:49:04 -0800

    Part of L4 3rd challenge

--- M10.twin_stick_workbook/Lessons/sprite_2d.gd
+++ M10.twin_stick_workbook/Lessons/sprite_2d.gd
@@ -1,11 +1,31 @@
 extends Sprite2D
 
+const PLAYER_DOWN = preload("res://player/godot_bottom.png")
+const PLAYYER_DOWN_Right = preload("res://player/godot_bottom_right.png")
+const PLAYER_RIGHT = preload("res://player/godot_right.png")
+const PLAYER_UP = preload("res://player/godot_up.png")
+const PLAYER_UP_RIGHT = preload("res://player/godot_up_right.png")
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+const UP_RIGHT = Vector2.UP + Vector2.RIGHT
+const UP_LEFT = Vector2.UP + Vector2.LEFT
+const DOWN_RIGHT = Vector2.DOWN + Vector2.RIGHT
+const DOWN_LEFT = Vector2.DOWN + Vector2.LEFT
 
+func _physics_process(_delta: float) -> void:
+	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
+	var direction_discrete := direction.sign()
+	match direction_discrete:
+		Vector2.RIGHT, Vector2.LEFT:
+			Sprite2D.texture = PLAYER_RIGHT
+		Vector2.UP:
+			Sprite2D.texture = PLAYER_UP
+		Vector2.DOWN:
+			Sprite2D.texture = PLAYER_DOWN
+		UP_RIGHT, UP_LEFT:
+			Sprite2D.texture = PLAYER_UP_RIGHT
+		DOWN_RIGHT, DOWN_LEFT:
+			Sprite2D.texture = PLAYER_UP_RIGHT
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	if direction_discrete.length() > 0:
+		Sprite2D.flip_h = direction.x < 0.0
