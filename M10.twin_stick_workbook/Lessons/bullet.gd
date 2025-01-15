7f35ac33df7e919c2aec78cc6269f47ea6b8917c
yaliu10 <yaliu10@s.sfusd.edu>
2025-01-15 13:10:29 -0800

    L5 challenges completed

---
+++ M10.twin_stick_workbook/Lessons/bullet.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+@export var speed := 700.0
+
+var _distance_traveled := 0.0
+var max_range := 900.0
+
+func _physics_process(delta: float) -> void:
+	var distance := speed * delta
+	var direction := Vector2.RIGHT.rotated(rotation) * distance
+	
+	position += direction
+	
+	_distance_traveled += distance
+	if _distance_traveled >= max_range:
+		_destroy()
+
+func _destroy():
+	queue_free()
