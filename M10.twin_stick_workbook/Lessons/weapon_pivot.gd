a3c2333eb30699f44fe5fe4855cabf716cc97d55
Celine <hewong4@s.sfusd.edu>
2025-01-22 13:24:25 -0800

    M10L7 complete

--- M10.twin_stick_workbook/Lessons/weapon_pivot.gd
+++ M10.twin_stick_workbook/Lessons/weapon_pivot.gd
@@ -1,18 +1,17 @@
 extends Node2D
 
-var input_trigged := false
+var is_using_gamepad := false
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
+@onready var weapon_anchor: Marker2D = %WeaponAnchor
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var direction := Vector2.ZERO
-	if input_trigged:
-		direction = global_position.direction_to(get_global_mouse_position())
-	else:
-		direction := Input.get_vector("move_down", "move_left", "move_right", "move_up")
-	if direction.length() > 0.1:
-		rotation = input_action.angle()
+	var cursor_position = get_global_mouse_position()
+	var rotation_angle = global_position.angle_to_point(cursor_position)
+	rotation = rotation_angle
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion or event is InputEventKey:
+		is_using_gamepad = false
+	elif event is InputEventJoypadButton or event is InputEventJoypadMotion:
+		is_using_gamepad = true
