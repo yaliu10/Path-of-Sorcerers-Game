6d253c237ec3265c43727a72fadd47ce9578e9cc
yaliu10 <yaliu10@s.sfusd.edu>
2025-01-27 15:46:13 -0800

    L10 3rd challenge in progress

--- M10.twin_stick_workbook/Lessons/player.gd
+++ M10.twin_stick_workbook/Lessons/player.gd
@@ -1,9 +1,26 @@
 class_name Player extends CharacterBody2D
 
 var max_speed := 600.0
+var health := max_health : set = set_health
 
+@export var max_health := 10
+
+@onready var collision_shape_2d: CollisionShape2D = %CollisionShape2D
+@onready var health_bar: ProgressBar = %HealthBar
+
+func _ready() -> void:
+	health_bar.max_value = max_health
+	health_bar.value = health
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * max_speed
 	move_and_slide()
+
+func set_health(new_health: int) -> void:
+	health = clampi(new_health, 0, max_health)
+	if health == 0:
+		die()
+
+func die() -> void:
+	queue_free()
